97c54e9d7ca0182549b7a6ec71a2c4a986683542
Anas <Anas@ANAS-PC>
2018-03-24 01:02:05 +0500

    Add project files.

---
+++ ArtsHub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtsHub.Startup))]
+namespace ArtsHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
